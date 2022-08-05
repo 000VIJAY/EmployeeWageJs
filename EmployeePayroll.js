9fdf419bf0208e8fe1202740861573ac1e480568
000VIJAY <vijay893689@gmail.com>
2022-08-05 17:58:27 +0530

    Vijay Added extended payroll data

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -3,16 +3,26 @@ class EmployeeePayrollData
     id;
     salary;
     name;
-    constructor(id,salary,name)
+    constructor(...params)
     {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
+        get gender() {return this._gender;}
+        set gender(gender){this._gender = gender;}
+        get startDate() {return this._startDate;}
+        set startDate(startDate) {this._startDate = startDate;}
     toString()
     {
-        return "id = "+this.id+", name = "+this.name+", salary = "+this.salary ; 
+        const option = {year: 'numeric' , month: 'long', day: 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined":new Date().toLocaleDateString("en-us",option);
+        return "id = "+this.id+", name = "+this.name+", salary = "+this.salary +", Gender = "+this.gender+" , StartDate = "+empDate;  
     }
 }
 let employeePayrolldata = new EmployeeePayrollData(1,'Vijay',50000);
 console.log(employeePayrolldata.toString());
+let newEmployeePayrolldata = new EmployeeePayrollData(1,'Shiva',50000,"M",new Date());
+console.log(newEmployeePayrolldata.toString());
