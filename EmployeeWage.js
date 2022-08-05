992aa638f44b4e428124fdd3d09c61588a4d735d
000VIJAY <vijay893689@gmail.com>
2022-08-05 15:58:29 +0530

    Vijay program for calculate wage per day

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,22 @@
-let isPresent = 1;
-let empCheck =  Math.floor(Math.random() * 10) % 2;
-if(empCheck==isPresent)
+let isPartTime = 1;
+let isFullTime = 2;
+const partTimeWorkingHrPerDay = 4;
+const fullTimeWorkingHr = 8;
+const wagePerHr = 20;
+let empWage =0;
+let empCheck =  Math.floor(Math.random() * 10) % 3;
+if(empCheck==isFullTime)
 { 
-      console.log("Employee is Present");
-    return;
+      console.log("Employee is full time");
+      empWage = fullTimeWorkingHr * wagePerHr;
+}
+else if(empCheck===isPartTime)
+{
+    console.log("Employee is part time");
+    empWage = partTimeWorkingHrPerDay * wagePerHr;
 }
 else
 {
     console.log("Employee is Absent");
 }
+console.log("Employee Wage per day: "+empWage);
